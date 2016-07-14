d0ff9ff498eeafa4eb0e913457243e0fda986efd
katieemelianova <katie_emelianova@hotmail.com>
2016-07-14 09:44:25 +0100

    corrected patch for run review

--- bin/automatic_review.py
+++ bin/automatic_review.py
@@ -135,11 +135,13 @@ class AutomaticRunReview():
         self.run_element_by_lane = run_element_by_lane
         self.cfg = cfg
 
+
     def patch_entry(self):
+
         for lane in self.run_element_by_lane:
             lane_number = (lane.get('lane_number'))
             lane_review, reasons = metrics(lane, self.cfg)
-            rest_communication.patch_entries('run_elements', payload={'reviewed':'pass'}, update_lists=None, where={"run_id":self.run_name, "lane": lane_number})
+            rest_communication.patch_entries('run_elements', payload={'reviewed':lane_review}, update_lists=None, where={"run_id":self.run_name, "lane": lane_number})
             if reasons:
                 rest_communication.patch_entries('run_elements', payload={ELEMENT_REVIEW_COMMENTS:'failed due to ' + ', '.join(reasons)}, update_lists=None, where={"run_id":self.run_name, "lane": lane_number})
 
@@ -184,6 +186,7 @@ def get_reviewable_samples():
     print(len(samples))
     if samples:
         for sample in samples:
+            print(sample)
             sample_id = sample.get('sample_id')
             species = clarity.get_species_from_sample(sample_id)
             sample_cfg = sample_config(sample, species)
