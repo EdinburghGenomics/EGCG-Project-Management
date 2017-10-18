5b6a5bb401c37597087c150b84bc3238403b9dee
tcezard <timothee.cezard@ed.ac.uk>
2017-10-18 11:48:11 +0100

    code reformat

--- tests/test_project_report.py
+++ tests/test_project_report.py
@@ -1,10 +1,10 @@
 import os
+import re
 from unittest.mock import Mock, patch, PropertyMock
 
-import re
+from egcg_core.config import cfg
 
 from project_report import ProjectReport
-from egcg_core.config import cfg
 from tests import TestProjectManagement
 
 
@@ -17,18 +17,25 @@ class FakeSample:
         self.name = name
         self.udf = udf
 
+
 fake_lims_sample1 = FakeSample(name='sample:01', udf={'Prep Workflow': None, 'Species': 'Thingius thingy'})
 fake_lims_sample2 = FakeSample(name='sample:02', udf={'Prep Workflow': None, 'Species': 'Thingius thingy'})
-fake_lims_sample3 = FakeSample(name='human_truseq_nano_sample_1', udf={'Prep Workflow': 'TruSeq Nano DNA Sample Prep', 'Species': 'Homo sapiens'})
-fake_lims_sample4 = FakeSample(name='non_human_truseq_nano_sample_1', udf={'Prep Workflow': 'TruSeq Nano DNA Sample Prep', 'Species': 'Thingius thingy'})
-fake_lims_sample5 = FakeSample(name='human_pcr_free_sample_1', udf={'Prep Workflow': 'TruSeq PCR-Free DNA Sample Prep', 'Species': 'Homo sapiens'})
-fake_lims_sample6 = FakeSample(name='non_human_pcr_free_sample_1', udf={'Prep Workflow': 'TruSeq PCR-Free DNA Sample Prep', 'Species': 'Thingius thingy'})
+fake_lims_sample3 = FakeSample(name='human_truseq_nano_sample_1',
+                               udf={'Prep Workflow': 'TruSeq Nano DNA Sample Prep', 'Species': 'Homo sapiens'})
+fake_lims_sample4 = FakeSample(name='non_human_truseq_nano_sample_1',
+                               udf={'Prep Workflow': 'TruSeq Nano DNA Sample Prep', 'Species': 'Thingius thingy'})
+fake_lims_sample5 = FakeSample(name='human_pcr_free_sample_1',
+                               udf={'Prep Workflow': 'TruSeq PCR-Free DNA Sample Prep', 'Species': 'Homo sapiens'})
+fake_lims_sample6 = FakeSample(name='non_human_pcr_free_sample_1',
+                               udf={'Prep Workflow': 'TruSeq PCR-Free DNA Sample Prep', 'Species': 'Thingius thingy'})
 
-fake_lims_sample_list = [fake_lims_sample1, fake_lims_sample2, fake_lims_sample3, fake_lims_sample4, fake_lims_sample5, fake_lims_sample6]
+fake_lims_sample_list = [fake_lims_sample1, fake_lims_sample2, fake_lims_sample3, fake_lims_sample4, fake_lims_sample5,
+                         fake_lims_sample6]
 fake_lims_sample_dict = dict([(s.name, s) for s in fake_lims_sample_list])
 
 fake_lims_project = Mock(
-    udf={'Project Title': 'a_research_title', 'Enquiry Number': '1337', 'Quote No.': '1338', 'Number of Quoted Samples': 2},
+    udf={'Project Title': 'a_research_title', 'Enquiry Number': '1337', 'Quote No.': '1338',
+         'Number of Quoted Samples': 2},
     researcher=Mock(first_name='First', last_name='Last', email='first.last@email.com')
 )
 
@@ -42,6 +49,7 @@ fake_db_sample_nonhum_nano = {'sample_id': 'human_pcr_free_sample_1'}
 fake_db_sample_nonhum_pcrfree = {'sample_id': 'non_human_pcr_free_sample_1'}
 fake_db_samples = [fake_db_sample1, fake_db_sample2]
 
+
 def get_fake_sample(sample_name):
     s = fake_lims_sample_dict.get(sample_name)
     if not s:
@@ -49,14 +57,17 @@ def get_fake_sample(sample_name):
         s = fake_lims_sample_dict.get(sample_name_sub)
     return s
 
+
 patch_get_sample = patch('egcg_core.clarity.get_sample', new=get_fake_sample)
 
+
 def get_patch_delivered_samples(samples):
-        return patch.object(
-            ProjectReport,
-            'delivered_samples_for_project',
-            new=PropertyMock(return_value=samples)
-        )
+    return patch.object(
+        ProjectReport,
+        'delivered_samples_for_project',
+        new=PropertyMock(return_value=samples)
+    )
+
 
 class TestProjectReport(TestProjectManagement):
     def setUp(self):
@@ -80,12 +91,10 @@ class TestProjectReport(TestProjectManagement):
             self.pr.delivered_samples_for_project
             mock_get.assert_called_once_with('samples', where={'project_id': 'a_project_name', 'delivered': 'yes'})
 
-
     def test_get_library_workflow(self):
         with patch_get_sample:
             assert self.pr.get_library_workflow_from_sample('sample:01') is None
             assert self.pr.get_library_workflow_from_sample('human_truseq_nano_sample_1') == 'TruSeq Nano DNA Sample Prep'
-
 
     def test_get_report_type(self):
         with patch_get_sample:
@@ -160,7 +169,6 @@ class TestProjectReport(TestProjectManagement):
 
     @patch(ppath('ProjectReport.get_folder_size'), return_value=1337000000000)
     def test_get_sample_info(self, mocked_project_size):
-
         with get_patch_delivered_samples(fake_db_samples), patch_get_project:
             print(self.pr.params)
             assert self.pr.get_sample_info() == [
@@ -200,7 +208,8 @@ class TestProjectReport(TestProjectManagement):
 def test_project_types():
     os.chdir(TestProjectManagement.root_path)
     projects = ('human_truseq_nano', 'human_pcr_free', 'non_human_truseq_nano', 'non_human_pcr_free')
-    samples = (fake_db_sample_hum_nano, fake_db_sample_hum_pcrfree, fake_db_sample_nonhum_nano, fake_db_sample_nonhum_pcrfree)
+    samples = (
+    fake_db_sample_hum_nano, fake_db_sample_hum_pcrfree, fake_db_sample_nonhum_nano, fake_db_sample_nonhum_pcrfree)
     for i, sample in enumerate(samples):
         with get_patch_delivered_samples([sample]), patch_get_sample, patch_get_project:
             pr = ProjectReport(projects[i])
