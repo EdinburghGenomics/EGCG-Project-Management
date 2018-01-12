cdf6625e266b7f346b515814eed264d5a39e1af7
Murray Wham <murray.wham@ed.ac.uk>
2018-01-12 16:02:22 +0000

    Adding integration test for delivered data deletion

--- integration_tests/test_delete_data/test_delete_data.py
+++ integration_tests/test_delete_data/test_delete_data.py
@@ -1,9 +1,11 @@
 import os
+from time import sleep
 from shutil import rmtree
+from collections import defaultdict
 from unittest.mock import patch
 from contextlib import contextmanager
 from integration_tests import IntegrationTest, integration_cfg
-from egcg_core import rest_communication
+from egcg_core import rest_communication, archive_management
 from egcg_core.config import cfg
 from data_deletion import client
 
@@ -27,7 +29,14 @@ def patches():
         p.stop()
 
 
-class TestDeleteRawData(IntegrationTest):
+class TestDeletion(IntegrationTest):
+    @staticmethod
+    def _run_main(argv):
+        with patches():
+            client.main(argv + ['--work_dir', work_dir])
+
+
+class TestDeleteRawData(TestDeletion):
     raw_dir = os.path.join(work_dir, 'raw')
     archive_dir = os.path.join(work_dir, 'archives')
 
@@ -69,11 +78,6 @@ class TestDeleteRawData(IntegrationTest):
             {'run_id': 'a_run', 'analysis_driver_procs': ['a_proc']}
         )
 
-    @staticmethod
-    def _run_main(argv):
-        with patches():
-            client.main(argv + ['--work_dir', work_dir])
-
     def test_raw_data(self):
         run_dir = os.path.join(self.raw_dir, 'a_run')
         assert os.path.isdir(run_dir)
@@ -95,3 +99,103 @@ class TestDeleteRawData(IntegrationTest):
         # nothing should have happened
         assert os.path.isdir(run_dir)
         assert not os.path.isdir(os.path.join(self.archive_dir, 'a_run'))
+
+
+class TestDeleteDeliveredData(TestDeletion):
+    fastq_dir = os.path.join(work_dir, 'fastqs')
+    fastq_archive_dir = os.path.join(work_dir, 'fastq_archives')
+    processed_data_dir = os.path.join(work_dir, 'processed_data')
+    processed_archive_dir = os.path.join(work_dir, 'processed_archives')
+    delivered_data_dir = os.path.join(work_dir, 'delivered_data')
+
+    @classmethod
+    def setUpClass(cls):
+        cfg.content = {
+            'executor': integration_cfg['executor'],
+            'data_deletion': {
+                'fastqs': cls.fastq_dir,
+                'fastq_archives': cls.fastq_archive_dir,
+                'processed_data': cls.processed_data_dir,
+                'processed_archives': cls.processed_archive_dir,
+                'delivered_data': cls.delivered_data_dir
+            }
+        }
+
+    def setUp(self):
+        super().setUp()
+
+        for d in (self.processed_data_dir, self.delivered_data_dir, self.fastq_dir):
+            if os.path.isdir(d):
+                rmtree(d)
+
+        self.all_files = defaultdict(list)
+        for i in range(1, 4):
+            sample_id = 'sample_' + str(i)
+            ext_sample_id = 'ext_' + sample_id
+            sample_dir = os.path.join(self.processed_data_dir, 'a_project', sample_id)
+            delivered_dir = os.path.join(self.delivered_data_dir, 'a_project', 'a_delivery_date', sample_id)
+            fastq_dir = os.path.join(self.fastq_dir, 'a_run', 'a_project', sample_id)
+
+            os.makedirs(sample_dir)
+            os.makedirs(fastq_dir)
+            os.makedirs(delivered_dir)
+
+            rest_communication.post_entry(
+                'samples',
+                {'sample_id': sample_id, 'user_sample_id': ext_sample_id, 'project_id': 'a_project'}
+            )
+            rest_communication.post_entry(
+                'run_elements',
+                {'run_element_id': 'a_run_%s_ATGC' % i, 'run_id': 'a_run', 'lane': i, 'barcode': 'ATGC',
+                 'project_id': 'a_project', 'sample_id': sample_id, 'library_id': 'a_library'}
+            )
+
+            for ext in ('.bam', '.vcf.gz'):
+                f = os.path.join(sample_dir, ext_sample_id + ext)
+                self.all_files[sample_id].append(f)
+
+            for r in ('1', '2'):
+                f = os.path.join(fastq_dir, 'L00%s_R%s.fastq.gz' % (i, r))
+                self.all_files[sample_id].append(f)
+
+            for f in self.all_files[sample_id]:
+                open(f, 'w').close()
+                os.link(f, os.path.join(delivered_dir, os.path.basename(f)))
+                archive_management.register_for_archiving(f)
+
+        for sample_id in ('sample_1', 'sample_2', 'sample_3'):
+            for f in self.all_files[sample_id]:
+                while not archive_management.is_archived(f):
+                    sleep(10)
+
+    def test_manual_release(self):
+        for sample_id in ('sample_1', 'sample_2', 'sample_3'):
+            assert all(archive_management.is_archived(f) for f in self.all_files[sample_id])
+            assert rest_communication.get_document('samples', where={'sample_id': sample_id})['data_deleted'] == 'none'
+
+        self._run_main(
+            ['delivered_data', '--manual_delete', 'sample_1', 'sample_2', 'sample_3',
+             '--sample_ids', 'sample_1', 'sample_2']
+        )
+        for sample_id in ('sample_1', 'sample_2'):
+            assert all(archive_management.is_released(f) for f in self.all_files[sample_id])
+            assert rest_communication.get_document('samples', where={'sample_id': sample_id})['data_deleted'] == 'on lustre'
+
+        # sample 3 should not be released
+        assert all(archive_management.is_archived(f) and not archive_management.is_released(f) for f in self.all_files['sample_3'])
+        assert rest_communication.get_document('samples', where={'sample_id': 'sample_3'})['data_deleted'] == 'none'
+
+    def test_dry_run(self):
+        for sample_id in ('sample_1', 'sample_2', 'sample_3'):
+            assert all(archive_management.is_archived(f) for f in self.all_files[sample_id])
+            assert rest_communication.get_document('samples', where={'sample_id': sample_id})['data_deleted'] == 'none'
+
+        self._run_main(
+            ['delivered_data', '--manual_delete', 'sample_1', 'sample_2', 'sample_3',
+             '--sample_ids', 'sample_1', 'sample_2', '--dry_run']
+        )
+
+        # nothing should have happened
+        for sample_id in ('sample_1', 'sample_2', 'sample_3'):
+            assert all(archive_management.is_archived(f) for f in self.all_files[sample_id])
+            assert rest_communication.get_document('samples', where={'sample_id': sample_id})['data_deleted'] == 'none'
