06f1008baf1acd9aa09512de9397b57cd102f772
Luke Paul Buttigieg <Luke.Buttigieg@ed.ac.uk>
2019-03-08 15:41:00 +0000

    Incomplete: Adding adapted version of disk_space_usage_analysis scripts

---
+++ bin/disk_space_usage_analysis.py
@@ -0,0 +1,51 @@
+import csv
+import os
+import sys
+from collections import Counter
+from config import load_config
+
+from egcg_core.config import cfg
+from egcg_core.rest_communication import Communicator
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# This command is used throughout to retrieve the disk usage of each directory checked
+space_command = "du -ck "
+
+# This communicator will be used throughout to download data from the Reporting App Rest API
+c = Communicator((username, password), 'https://egreports.mvm.ed.ac.uk/api/0.1/')
+
+
+class RunDirectoryChecker:
+    def __init__(self):
+        self.directory_set = set()
+        self.sample_counter = Counter()
+        self.sample_splits = Counter()
+        self.deleted_dict = {}
+
+        self.bash_command = "find /lustre/edgeprod/processed/runs/*/*/*/*.fastq.gz -type f | egrep -v '/fastq/fastq'"
+
+
+def run_directory_check():
+
+
+# samples = c.get_documents('samples', projection={"data_deleted":1} ,max_results=1000, all_pages=True)
+
+# cfg['run']['output_dir']    # Run directories
+# cfg['sample']['output_dir'] # project directories
+
+
+def main():
+    a = argparse.ArgumentParser()
+    a.add_argument()
+
+    args = a.parse_args()
+    load_config()
+
+    logging_default.add_stdout_handler()
+    if args.debug:
+        logging_default.set_log_level(logging.DEBUG)
+
+
+if __name__ == '__main__':
+    main()
