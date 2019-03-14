31283793c867485c4f585964f8554762cc56341b
Luke Paul Buttigieg <Luke.Buttigieg@ed.ac.uk>
2019-03-14 15:42:59 +0000

    Incomplete: Adding adapted version of disk_space_usage_analysis scripts

--- bin/disk_space_usage_analysis.py
+++ bin/disk_space_usage_analysis.py
@@ -5,31 +5,39 @@ import os
 import sys
 from collections import Counter
 from config import load_config
-from egcg_core.app_logging import logging_default
 
+from egcg_core import rest_communication
+from egcg_core.app_logging import AppLogger, logging_default as log_cfg
 from egcg_core.config import cfg
-from egcg_core.rest_communication import Communicator
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# This command is used throughout to retrieve the disk usage of each directory checked
-space_command = "du -ck "
-
-# This communicator will be used throughout to download data from the Reporting App Rest API
-c = Communicator((username, password), 'https://egreports.mvm.ed.ac.uk/api/0.1/')
+from egcg_core.exceptions import EGCGError
 
 
-class RunDirectoryChecker:
+class DiskSpaceUsageAnalysis(AppLogger):
+    def __init__(self):
+        sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+        # This command is used throughout to retrieve the disk usage of each directory checked
+        self.space_command = "du -ck "
+
+        # Loading the run and project directory values from the config file
+        self.dir_cfg = cfg.query('runs_dir_space_analysis', 'projects_dir_space_analysis', ret_default={})
+        if set(self.dir_cfg) != {'runs_dir', 'projects_dir'}:
+            self.error('Directory config invalid or incomplete. Please ensure your config has an entry for '
+                       'runs_directory and projects_directory.')
+            return
+
+class RunDirectoryChecker(AppLogger):
     def __init__(self):
         self.directory_set = set()
         self.sample_counter = Counter()
         self.sample_splits = Counter()
         self.deleted_dict = {}
 
-        self.bash_command = "find /lustre/edgeprod/processed/runs/*/*/*/*.fastq.gz -type f | egrep -v '/fastq/fastq'"
+        self.bash_command = "find " + DiskSpaceUsageAnalysis.dir_cfg['runs_dir_space_analysis']['runs_dir'] \
+                            + ". -name '*.fastq.gz' -type f | egrep -v '/fastq/fastq'"
 
 
-def run_directory_check():
+    def run_directory_check():
 
 
 # samples = c.get_documents('samples', projection={"data_deleted":1} ,max_results=1000, all_pages=True)
@@ -39,19 +47,23 @@ def run_directory_check():
 
 
 def main():
-    a = argparse.ArgumentParser()
-    a.add_argument('all', help='Runs all available checks - '
+    arg_parser = argparse.ArgumentParser()
+    arg_group = arg_parser.add_mutually_exclusive_group(required=True)
+    arg_group.add_argument('all', help='Runs all available checks - '
                                'Run directory check, residual run and project directory checks.')
-    a.add_argument('run_directory', help='Check the run directory for space used storing samples, '
+    arg_group.add_argument('runs_directory', help='Check the runs directory for space used storing samples, '
                                          'and displays the respective archiving status.')
-    a.add_argument('residual_run_directory', help='Check space used when sample directories space used ')
+    arg_group.add_argument('residual_runs_directory', help='Check residual space used when samples directories space used '
+                                                          'is deducted from the respective runs directory space used.')
+    arg_group.add_argument('residual_projects_directory', help='Check residual space used when samples ')
 
-    args = a.parse_args()
+    args = arg_parser.parse_args()
     load_config()
+    log_cfg.set_log_level(logging.INFO)
+    log_cfg.add_stdout_handler()
 
-    logging_default.add_stdout_handler()
     if args.debug:
-        logging_default.set_log_level(logging.DEBUG)
+        log_cfg.set_log_level(logging.DEBUG)
 
 
 if __name__ == '__main__':
