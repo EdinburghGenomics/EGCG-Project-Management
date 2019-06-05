b99d4380c2f128c33d215d49d3e24d3233794cb3
Luke Paul Buttigieg <Luke.Buttigieg@ed.ac.uk>
2019-06-05 15:25:44 +0100

    Finished merging run_directory.py with residual_run_directory.py

--- disk_space_usage_analysis/run_directory_checker.py
+++ disk_space_usage_analysis/run_directory_checker.py
@@ -14,6 +14,8 @@ class RunDirectoryChecker(AppLogger):
         self.directory_set = set()
         self.sample_counter = Counter()
         self.sample_splits = Counter()
+        self.run_sample_counter = Counter()
+        self.run_counter = Counter()
         self.deleted_dict = {}
 
         self.bash_command = "find " + disk_space_usage_analysis.dir_cfg['runs_dir_space_analysis']['runs_dir'] \
@@ -28,20 +30,29 @@ class RunDirectoryChecker(AppLogger):
             if len(sample_directory_path_split) == 9:
                 try:
                     sample_name = sample_directory_path_split[7]
+                    run_directory_name = sample_directory_path_split[5]
                 except IndexError:
                     self.error('Index Error when splitting sample directory path.')
                     continue
 
+                run_directory_path = '/{lustre}/{env}/{proc}/{runs}/{directory}'.format(
+                    lustre=sample_directory_path_split[1], env=sample_directory_path_split[2],
+                    proc=sample_directory_path_split[3], runs=sample_directory_path_split[4],
+                    directory=sample_directory_path_split[5])
                 directory_path = '/{lustre}/{env}/{proc}/{runs}/{directory}/{project}/{sample}'.format(
                     lustre=sample_directory_path_split[1], env=sample_directory_path_split[2],
                     proc=sample_directory_path_split[3], runs=sample_directory_path_split[4],
                     directory=sample_directory_path_split[5], project=sample_directory_path_split[6],
                     sample=sample_directory_path_split[7])
+
                 if directory_path not in self.directory_set:
+                    # Not in directory set - adding it
                     self.directory_set.add(directory_path)
                     command = self.disk_space_usage_analysis.space_command + directory_path
                     command_output = os.popen(command).read()
+                    # Adding/Incrementing value in sample_splits, run_sample_counter and sample_counter
                     self.sample_splits.update({sample_name: 1})
+                    self.run_sample_counter.update({run_directory_name:int(command_output.split()[0])})
                     self.sample_counter.update({sample_name: int(command_output.split()[0])})
                     try:
                         data_deleted = rest_communication.get_document(
@@ -50,11 +61,17 @@ class RunDirectoryChecker(AppLogger):
                         data_deleted = "Error - not found"
                     self.deleted_dict[sample_name] = data_deleted
 
+                if run_directory_name not in self.run_counter:
+                    # Not in run directory set - adding it
+                    command = self.disk_space_usage_analysis.space_command + run_directory_path
+                    command_output = os.popen(command).read()
+                    self.run_counter.update({run_directory_name: int(command_output.split()[0])})
+
     # Exports the run directory analysis to a CSV file
     def export_run_directory_analysis_csv(self):
         with open(self.disk_space_usage_analysis.dir_cfg['runs_dir_space_analysis']['output_dir'] + 'run_dir_analysis.csv',
-                  mode='w') as analysis_file:
-            file_writer = csv.writer(analysis_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                  mode='w') as file:
+            file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             file_writer.writerow(['Sample Name', 'Size', 'Data Deleted', 'Splits'])
 
             for key, value in self.sample_counter.most_common():
@@ -64,7 +81,7 @@ class RunDirectoryChecker(AppLogger):
 
     # Exports the run directory analysis to a TXT file
     def export_run_directory_analysis_txt(self):
-        with open(self.disk_space_usage_analysis.dir_cfg['runs_dir_space_analysis']['output_dir'] + 'run_dir_analysis.txt',
+        with open(self.disk_space_usage_analysis.dir_cfg['runs_dir_space_analysis']['output_dir'] + 'run_dir_analysis_log.txt',
                   mode='w') as analysis_txt_file:
             analysis_txt_file.write('Samples and space taken as follows: ')
 
@@ -76,9 +93,32 @@ class RunDirectoryChecker(AppLogger):
             for key, value in self.sample_splits.most_common():
                 analysis_txt_file.write(key + ': ' + str(value))
 
+    # Exports the residual run directory analysis to a CSV file
+    def export_residual_run_directory_analysis(self):
+        with open(self.disk_space_usage_analysis.dir_cfg['runs_dir_space_analysis']['output_dir'] + 'residual_run_directory_analysis.csv',
+                  mode='w') as file:
+            file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            file_writer.writerow(['Run Directory Name', 'Residual Size'])
+
+            for key, value in self.run_counter.most_common():
+                residual_space = value - self.run_sample_counter[key]
+                file_writer.writerow([key, str(residual_space)])
+
+    def export_residual_run_directory_analysis_txt(self):
+        with open(self.disk_space_usage_analysis.dir_cfg['runs_dir_space_analysis']['output_dir'] + 'residual_run_dir_analysis_log.txt',
+                  mode='w') as analysis_txt_file:
+            analysis_txt_file.write('Run directories space taken as follows: ')
+            for key, value in self.run_counter.most_common():
+                analysis_txt_file.write(key + ': ' + str(value))
+
+            analysis_txt_file.write('Sample run directories took space as follows: ')
+            for key, value in self.run_sample_counter.most_common():
+                analysis_txt_file.write(key + ': ' + str(value))
+
     def execute(self):
         self.run_directory_check()
         self.export_run_directory_analysis_csv()
+        self.export_residual_run_directory_analysis()
         self.export_run_directory_analysis_txt()
 
 # samples = c.get_documents('samples', projection={"data_deleted":1} ,max_results=1000, all_pages=True)
