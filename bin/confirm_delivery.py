5fff29601fcacb26d9f8c330ade4731f44cbbbb1
Luke Paul Buttigieg <Luke.Buttigieg@ed.ac.uk>
2019-08-05 11:13:52 +0100

    Modified where check is

--- bin/confirm_delivery.py
+++ bin/confirm_delivery.py
@@ -29,10 +29,10 @@ class DeliveredSample(AppLogger):
         if self.sample_id.startswith('FD'):
             # might be FluidX tube barcode
             arts = clarity.connection().get_artifacts(type='Analyte', udf={'2D Barcode': self.sample_id})
+            samples = clarity.connection().get_samples(udf={'2D Barcode': self.sample_id})
             if arts and len(arts) == 1:
                 self.sample_id = arts[0].samples[0].name
             elif len(arts) == 0:
-                samples = clarity.connection().get_samples(udf={'2D Barcode': self.sample_id})
                 self.sample_id = samples[0].name
             else:
                 self.error('Found %s artifacts for FluidX sample %s', len(arts), self.sample_id)
