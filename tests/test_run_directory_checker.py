4227be4bb86470075e5ea9c6c14fa59aeab30df6
tcezard <timothee.cezard@ed.ac.uk>
2019-09-09 15:38:09 +0100

    Example of test

--- tests/test_run_directory_checker.py
+++ tests/test_run_directory_checker.py
@@ -1,22 +1,29 @@
+import os
+import shutil
 from unittest.mock import patch
 
 from bin.disk_space_usage_analyser import DiskSpaceUsageAnalysisHelper
 from disk_space_usage_analysis import RunsDirectoryChecker
 from egcg_core.config import cfg
 from tests import TestProjectManagement
+from tests.test_data_delivery import touch
 
 
 class TestRunDirectoryChecker(TestProjectManagement):
     """This test suite checks the functionality of the run directory checker."""
     def setUp(self):
-        cfg.content.update({
-                            'directory_space_analysis':
-                            {
-                                 'runs_dir': '/lustre/edgeprod/processed/runs/',
-                                 'projects_dir': '/lustre/edgeprod/processed/projects/',
-                                 'output_dir': '~/Documents/development/output/'
-                            }
-                         })
+        self.disk_usage_dir = os.path.join(self.assets_path,  'disk_space_usage')
+        runs_dir = os.path.join(self.disk_usage_dir, 'runs')
+        projects_dir = os.path.join(self.disk_usage_dir, 'projects')
+        output_dir = os.path.join(self.disk_usage_dir, 'output')
+        os.makedirs(runs_dir, exist_ok=True)
+        os.makedirs(projects_dir, exist_ok=True)
+        os.makedirs(output_dir, exist_ok=True)
+        cfg.content['directory_space_analysis'] = {}
+        cfg.content['directory_space_analysis']['runs_dir'] = runs_dir
+        cfg.content['directory_space_analysis']['projects_dir'] = projects_dir
+        cfg.content['directory_space_analysis']['output_dir'] = output_dir
+
         self.disk_usage_helper = DiskSpaceUsageAnalysisHelper()
         self.response = [
             {
@@ -32,6 +39,9 @@ class TestRunDirectoryChecker(TestProjectManagement):
                 'data_deleted': 'none'
             }
         ]
+        os.makedirs(os.path.join(runs_dir, 'project1', 'sample1'), exist_ok=True)
+        touch(os.path.join(runs_dir, 'project1', 'sample1', 'sample1_R1.fastq.gz'), content='A fastq file')
+        touch(os.path.join(runs_dir, 'project1', 'sample1', 'sample1_R2.fastq.gz'), content='A fastq file')
 
     def test_debug_logging_level_true(self):
         with patch('egcg_core.rest_communication.get_documents', return_value=self.response):
@@ -39,4 +49,4 @@ class TestRunDirectoryChecker(TestProjectManagement):
 
     def tearDown(self) -> None:
         """Remove files created during testing process"""
-        pass
+        shutil.rmtree(self.disk_usage_dir)
