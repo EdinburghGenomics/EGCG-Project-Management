3f252059467a62faa7381ff742150c20e8cd9d6f
Luke Paul Buttigieg <Luke.Buttigieg@ed.ac.uk>
2019-09-09 16:22:40 +0100

    Merged suggestions by TC

--- tests/test_run_directory_checker.py
+++ tests/test_run_directory_checker.py
@@ -1,18 +1,22 @@
+import os
+import shutil
 from unittest.mock import patch
 
 from bin.disk_space_usage_analyser import DiskSpaceUsageAnalysisHelper
 from disk_space_usage_analysis import RunsDirectoryChecker
 from egcg_core.config import cfg
 from tests import TestProjectManagement
+from tests.test_data_delivery import touch
 
 
 class TestRunDirectoryChecker(TestProjectManagement):
     """This test suite checks the functionality of the run directory checker."""
-    def setUp(self):
-        self.disk_usage_dir = os.path.join(self.assets_path,  'disk_space_usage')
-        runs_dir = os.path.join(self.disk_usage_dir, 'runs')
-        projects_dir = os.path.join(self.disk_usage_dir, 'projects')
-        output_dir = os.path.join(self.disk_usage_dir, 'output')
+    @classmethod
+    def setUpClass(cls):
+        cls.disk_usage_dir = os.path.join(cls.assets_path, 'disk_space_usage')
+        runs_dir = os.path.join(cls.disk_usage_dir, 'runs')
+        projects_dir = os.path.join(cls.disk_usage_dir, 'projects')
+        output_dir = os.path.join(cls.disk_usage_dir, 'output')
         os.makedirs(runs_dir, exist_ok=True)
         os.makedirs(projects_dir, exist_ok=True)
         os.makedirs(output_dir, exist_ok=True)
@@ -21,20 +25,7 @@ class TestRunDirectoryChecker(TestProjectManagement):
         cfg.content['directory_space_analysis']['projects_dir'] = projects_dir
         cfg.content['directory_space_analysis']['output_dir'] = output_dir
 
-        self.disk_usage_helper = DiskSpaceUsageAnalysisHelper()
-        self.response = [
-    # TODO: Change directories prior to final commit
-    @classmethod
-    def setUpClass(cls):
-        print('Setting up class')
-        cfg.content.update({
-                            'directory_space_analysis':
-                            {
-                                 'runs_dir': '/lustre/edgeprod/processed/runs/',
-                                 'projects_dir': '/lustre/edgeprod/processed/projects/',
-                                 'output_dir': '/Users/lbuttigi/Documents/development/output/'
-                            }
-                         })
+        cls.disk_usage_helper = DiskSpaceUsageAnalysisHelper()
 
         cls.response = [
             {
@@ -64,4 +55,4 @@ class TestRunDirectoryChecker(TestProjectManagement):
     @classmethod
     def tearDownClass(cls):
         """Remove files created during testing process"""
-        shutil.rmtree(self.disk_usage_dir)
+        shutil.rmtree(cls.disk_usage_dir)
